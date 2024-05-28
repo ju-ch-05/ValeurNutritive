2db52b7a23e3ffe2711182ee48ce954ca3b02424
Julien Chauvette <julien.chauvette2005@gmail.com>
2024-05-28 14:31:39 -0400

    Traitement stocke les données dans le local storage.

--- src/traitements.js
+++ src/traitements.js
@@ -1,33 +1,36 @@
-var quantite ;
-var calories;
-var lipides;
-var grasSat;
-var grasTrans;
-var glucides;
-var fibres;
-var sucres;
-var proteines;
-var cholesterol;
-var sodium;
-var calcium;
-var fer;
-
 function assignationVariables() {
-    quantite = document.getElementById("proportions").value;
-    calories = document.getElementById("calories").value;
-    lipides = document.getElementById("lipides").value;
-    grasSat = document.getElementById("saturés").value;
-    grasTrans = document.getElementById("trans").value;
-    glucides = document.getElementById("glucides").value;
-    fibres = document.getElementById("fibres").value;
-    sucres = document.getElementById("sucres").value;
-    proteines = document.getElementById("protéines").value;
-    cholesterol = document.getElementById("cholestérol").value;
-    sodium = document.getElementById("sodium").value;
-    calcium = document.getElementById("calcium").value;
-    fer = document.getElementById("fer").value;
-}
+    var quantite = document.getElementById("proportions").value;
+    var calories = document.getElementById("calories").value;
+    var lipides = document.getElementById("lipides").value;
+    var grasSat = document.getElementById("saturés").value;
+    var grasTrans = document.getElementById("trans").value;
+    var glucides = document.getElementById("glucides").value;
+    var fibres = document.getElementById("fibres").value;
+    var sucres = document.getElementById("sucres").value;
+    var proteines = document.getElementById("protéines").value;
+    var cholesterol = document.getElementById("cholestérol").value;
+    var sodium = document.getElementById("sodium").value;
+    var calcium = document.getElementById("calcium").value;
+    var fer = document.getElementById("fer").value;
 
- function remplirTableau() {
-    document.getElementById("portion").innerHTML = "Per "+ quantite + " g<br>pour " + quantite + " g";
- }
+    class Aliment {
+        constructor(quantite, calories, lipides, grasSat, grasTrans,
+            glucides, fibres, sucres, proteines, cholesterol, sodium, calcium) {
+            this.quantite = quantite;
+            this.calories = calories;
+            this.lipides = lipides;
+            this.grasSat = grasSat;
+            this.grasTrans = grasTrans;
+            this.glucides = glucides;
+            this.fibres = fibres;
+            this.sucres = sucres;
+            this.proteines = proteines;
+            this.cholesterol = cholesterol;
+            this.sodium = sodium;
+            this.calcium = calcium;
+        }
+    }
+    
+    localStorage.setItem("aliment", JSON.stringify(new Aliment(quantite, calories, lipides, grasSat, 
+        grasTrans, glucides, fibres, sucres, proteines, cholesterol, sodium, calcium)));
+}
