af4f37011d6dca4b0abf18ea47e0b5da9d4d9ab5
Julien Chauvette <julien.chauvette2005@gmail.com>
2024-05-28 18:44:57 -0400

    Ajout de la classe ValeurQuotidienne pour calculé le pourcentage à partir de la quantité.

--- src/traitements.js
+++ src/traitements.js
@@ -1,3 +1,41 @@
+class Aliment {
+    constructor(quantite, calories, lipides, grasSat, grasTrans,
+        glucides, fibres, sucres, proteines, cholesterol, sodium, calcium, fer) {
+        this.quantite = quantite;
+        this.calories = calories;
+        this.lipides = lipides;
+        this.grasSat = grasSat;
+        this.grasTrans = grasTrans;
+        this.glucides = glucides;
+        this.fibres = fibres;
+        this.sucres = sucres;
+        this.proteines = proteines;
+        this.cholesterol = cholesterol;
+        this.sodium = sodium;
+        this.calcium = calcium;
+        this.fer = fer;
+    }
+}
+
+const aNRefLipides = 75;
+const aNRefLipidesSat = 20;
+const aNRefFibres = 28;
+const aNRefSucres = 100;
+const aNRefSodium = 2300;
+const aNRefCalcium = 1300;
+const aNRefFer = 18;
+class valeurQuotidienne {
+    constructor(aliment) {
+        this.vqLipides = (aliment.lipides / aNRefLipides) * 100;
+        this.vqLipidesSat = ((aliment.grasSat + aliment.grasTrans) / aNRefLipidesSat) * 100;
+        this.vqFibres = (aliment.fibres / aNRefFibres) * 100;
+        this.vqSucres = (aliment.sucres / aNRefSucres) * 100;
+        this.vqSodium = (aliment.sodium / aNRefSucres) * 100;
+        this.vqCalcium = (aliment.calcium / aNRefCalcium) * 100;
+        this.vqFer = (aliment.fer / aNRefFer) * 100;
+    }
+}
+
 function assignationVariables() {
     var quantite = document.getElementById("proportions").value;
     var calories = document.getElementById("calories").value;
@@ -13,25 +51,9 @@ function assignationVariables() {
     var calcium = document.getElementById("calcium").value;
     var fer = document.getElementById("fer").value;
 
-    class Aliment {
-        constructor(quantite, calories, lipides, grasSat, grasTrans,
-            glucides, fibres, sucres, proteines, cholesterol, sodium, calcium) {
-            this.quantite = quantite;
-            this.calories = calories;
-            this.lipides = lipides;
-            this.grasSat = grasSat;
-            this.grasTrans = grasTrans;
-            this.glucides = glucides;
-            this.fibres = fibres;
-            this.sucres = sucres;
-            this.proteines = proteines;
-            this.cholesterol = cholesterol;
-            this.sodium = sodium;
-            this.calcium = calcium;
-            this.fer = fer;
-        }
-    }
-    
-    localStorage.setItem("aliment", JSON.stringify(new Aliment(quantite, calories, lipides, grasSat, 
-        grasTrans, glucides, fibres, sucres, proteines, cholesterol, sodium, calcium, fer)));
+    var aliment = new Aliment(quantite, calories, lipides, grasSat, 
+        grasTrans, glucides, fibres, sucres, proteines, cholesterol, sodium, calcium, fer);
+
+    localStorage.setItem("aliment", JSON.stringify(aliment));
+    localStorage.setItem("valeurQuotidienne", JSON.stringify(new valeurQuotidienne(aliment)));
 }
